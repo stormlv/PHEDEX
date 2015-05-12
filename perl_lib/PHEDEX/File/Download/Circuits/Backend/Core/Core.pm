9601f06a6797faddf768de033641155c6655bc9b
Vlad Lapadatescu <vlad@cern.ch>
2015-05-12 10:04:02 +0200

    UPDATED: Core to use Moose and helper classes

--- perl_lib/PHEDEX/File/Download/Circuits/Backend/Core/Core.pm
+++ perl_lib/PHEDEX/File/Download/Circuits/Backend/Core/Core.pm
@@ -1,82 +1,62 @@
 package PHEDEX::File::Download::Circuits::Backend::Core::Core;
 
-use strict;
-use warnings;
+use Moose;
 
 use base 'PHEDEX::Core::Logging';
+
 use POE;
 use List::Util qw(min);
 
-my %params =
-	(
-        AGENT_TRANSLATION           =>  {},         # Stores the PhEDEx names to IDC ref
-        SUPPORTED_RESOURCE          =>  undef,      # Set this in extending class
-	);
-		
-sub new
-{
-    my $proto = shift;
-    my $class = ref($proto) || $proto;
+use PHEDEX::File::Download::Circuits::Helpers::Utils::Utils;
+use PHEDEX::File::Download::Circuits::ManagedResource::Core::Path;
 
-    my %args = (@_);
-    #   use 'defined' instead of testing on value to allow for arguments which are set to zero.
-    map { $args{$_} = defined($args{$_}) ? $args{$_} : $params{$_} } keys %params;
-    my $self = $class->SUPER::new(%args);
+has 'availablePaths'    => (is  => 'rw', isa => 'HashRef[PHEDEX::File::Download::Circuits::Backend::Core::Path]',
+                            traits  => ['Hash'], 
+                            handles => {getPathByName   => 'get',
+                                        getPaths        => 'values',
+                                        pathExists      => 'exists',
+                                        deletePath      => 'delete'});
 
-    bless $self, $class;
-    return $self;
+sub addPath {
+    my ($self, $path) = @_;
+    return undef if ! defined $path;
+    $self->paths->{$path->getName} = $path;
+    return $path;
 }
 
 # Initialize all POE events specifically related to circuits
-sub _poe_init
-{
-  my ($self, $kernel, $session) = @_;
-
-  # Declare events which are going to be used by the CircuitManager
-  my @poe_subs = qw(backendRequestCircuit backendTeardownCircuit);
-
+sub _poe_init {
+    my ($self, $kernel, $session) = @_;
+    # Declare events which are going to be used by the ResourceManager
+    my @poe_subs = qw(backendRequestResource backendUpdateResource backendTeardownResource);
   $kernel->state($_, $self) foreach @poe_subs;
 }
 
-# Check if the node supports circuits. This shouldn't be called by the CircuitManager.
-# Normally it should only be interested in the link itself. For that, use 'check_link_support'
-sub checkNodeSupport {
-    my ($self, $node) = @_;
-    return (defined $node && $self->{AGENT_TRANSLATION}{$node}) ? 1 : 0;
-}
-
-# Checks if a link supports the creation of circuits
-sub checkLinkSupport {
-    my ($self, $from_node, $to_node) = @_;
-    return $self->checkNodeSupport($from_node) && $self->checkNodeSupport($to_node);
-}
-
-# Returns the advertised bandwidth that can be had between the two provided nodes
-sub getCircuitBandwidth {
-    my ($self, $from_node, $to_node) = @_;
-    return unless $self->checkLinkSupport($from_node, $to_node);
-    return min $self->{AGENT_TRANSLATION}{$from_node}{BANDWIDTH}, $self->{AGENT_TRANSLATION}{$to_node}{BANDWIDTH};
+sub getPathByNodes {
+    my ($self, $nodeA, $nodeB, $bidirectional) = @_;
+    my $pathName = &getPath($nodeA, $nodeB, $bidirectional);
+    return $self->getPath($pathName);
 }
 
 # This method should be implemented by the backend child
-# It will be called to request the creation of a circuit
-sub backendRequestCircuit {
+# It will be called to request the creation of a resource
+sub backendRequestResource {
     my $self = shift;
     $self->Fatal("request not implemented by circuit backend ", __PACKAGE__);
 }
 
 # This method should be implemented by the backend child
-# It will be called to request the teardown of a circuit
-sub backendTeardownCircuit {
+# It will be called update the resource
+sub backendUpdateResource {
     my $self = shift;
-    $self->Fatal("teardown not implemented by circuit backend ", __PACKAGE__);
+    $self->Fatal("request not implemented by backend ", __PACKAGE__);
 }
 
 # This method should be implemented by the backend child
-# It will be called update the bandwidth
-sub backendUpdateBandwidth {
+# It will be called to request the teardown of a resource
+sub backendTeardownResource {
     my $self = shift;
-    $self->Fatal("request not implemented by backend ", __PACKAGE__);
+    $self->Fatal("teardown not implemented by circuit backend ", __PACKAGE__);
 }
 
 1;
