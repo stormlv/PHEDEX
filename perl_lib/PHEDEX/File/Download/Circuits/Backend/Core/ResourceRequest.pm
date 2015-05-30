ebe790b06856dedca6c960de40c4488c412d38e8
Vlad Lapadatescu <vlad@cern.ch>
2015-05-30 21:08:37 +0200

    ADDED: New class for requesting resources

---
+++ perl_lib/PHEDEX/File/Download/Circuits/Backend/Core/ResourceRequest.pm
@@ -0,0 +1,15 @@
+package PHEDEX::File::Download::Circuits::Backend::Core::ResourceRequest;
+
+use Moose;
+
+use base 'PHEDEX::Core::Logging';
+
+use PHEDEX::File::Download::Circuits::Common::Constants;
+
+has 'siteA'         => (is  => 'rw', isa => 'Str', required => 1);
+has 'siteB'         => (is  => 'rw', isa => 'Str', required => 1);
+has 'bidirectional' => (is  => 'rw', isa => 'Bool', default => 1);
+has 'lifetime'      => (is  => 'rw', isa => 'Num', default => 6*HOUR);
+has 'callback'      => (is  => 'rw', isa => 'Ref', required => 1);
+
+1;
